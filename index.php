1f0a4cc2560b2239717d943e03fb0f8fdf74a0eb
Clodial <ff17cloud@gmail.com>
2015-10-27 15:00:51 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+</body>
+	<script type="javascript/text" src="js/jquery-2.1.4.min.js"></script>
+</html>
